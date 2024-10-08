152594c0cc8370a73347879d774412557f682b0e
katixiik <pracakatix@gmail.com>
2024-10-08 19:16:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Gra na najlepszej sieci CZEKMC.PL","Cały czas sie ulepszam"];
+const statusMessages = ["Cały czas sie ulepszam"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
